88439f238455a57ff89bf52b083522ff36d6496b
Alexander <dante690dark@gmail.com>
2025-04-02 17:20:43 -0500

    Custom Video Player

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,73 @@
+/* Get Our Elements */
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const ranges = document.querySelectorAll('.player__slider');
+const fullscreenButton = document.querySelector('.fullscreen');
+
+
+/* Build out functions */
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() { 
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function toggleFullscreen() {
+  const isFullscreen = document.fullscreenElement || document.webkitFullscreenElement
+  const requestMethod = player.requestFullscreen || player.webkitRequestFullscreen || player.mozRequestFullScreen || player.msRequestFullscreen
+  const exitMethod = document.exitFullscreen || document.webkitExitFullscreen || document.mozCancelFullScreen || document.msExitFullscreen
+
+  if (!isFullscreen) {
+    requestMethod?.call(player)
+  } else {
+    exitMethod?.call(document)
+  }
+}
+
+/* Hook up the evemt listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+progress.addEventListener('mouseleave', () => mousedown = false);
+
+fullscreenButton.addEventListener('click', toggleFullscreen)
